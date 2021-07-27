96e37fbeb1cb77be0b4c5a2232e6309c8d943fa5
Juliya Strugatskaya <strugatskaya.um@gmail.com>
2021-07-27 22:44:47 +0300

    tests remastered for junit4

--- src/test/java/ru/netology/service/CachbackServiceJunit4Test.java
+++ src/test/java/ru/netology/service/CachbackServiceJunit4Test.java
@@ -2,33 +2,33 @@ package ru.netology.service;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class CachbackServiceJunit4Test {
     public CashbackHackService service = new CashbackHackService();
 
     @Test
     public void shouldCalculateIf900Positive() {
-        assertEquals(service.remain(900), 100);
+        assertEquals(100, service.remain(900));
     }
 
     @Test
     public void shouldCalculateIf1000Positive() {
-        assertEquals(service.remain(1000), 1000); //кажется, это обещанный баг
+        assertEquals(1000, service.remain(1000)); //кажется, это обещанный баг
     }
 
     @Test
     public void shouldCalculateIf1001Positive() {
-        assertEquals(service.remain(1001), 999); // зачем нам предлагает закупить на 999р, если у нас уже выше границы?
+        assertEquals(999, service.remain(1001)); // зачем нам предлагает закупить на 999р, если у нас уже выше границы?
     }
 
     @Test
     public void shouldCalculateIf0Positive() {
-        assertEquals(service.remain(0), 1000);
+        assertEquals(1000, service.remain(0));
     }
 
     @Test
     public void shouldCalculateIfNegative() {
-        assertEquals(service.remain(-1), 1001); //очень странно, что вообще можно купить на -1рубль :)
+        assertEquals(1001, service.remain(-1)); //очень странно, что вообще можно купить на -1рубль :)
     }
 }
