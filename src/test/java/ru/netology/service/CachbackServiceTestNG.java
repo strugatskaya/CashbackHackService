7e8975275557fd76dbd591839cdf940e5896d5d5
Juliya Strugatskaya <strugatskaya.um@gmail.com>
2021-07-27 22:00:00 +0300

    TestNG added

---
+++ src/test/java/ru/netology/service/CachbackServiceTestNG.java
@@ -0,0 +1,34 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class CachbackServiceTestNG {
+    public CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldCalculateIf900Positive() {
+        assertEquals(service.remain(900), 100);
+    }
+
+    @Test
+    public void shouldCalculateIf1000Positive() {
+        assertEquals(service.remain(1000), 1000); //кажется, это обещанный баг
+    }
+
+    @Test
+    public void shouldCalculateIf1001Positive() {
+        assertEquals(service.remain(1001), 999); // зачем нам предлагает закупить на 999р, если у нас уже выше границы?
+    }
+
+    @Test
+    public void shouldCalculateIf0Positive() {
+        assertEquals(service.remain(0), 1000);
+    }
+
+    @Test
+    public void shouldCalculateIfNegative() {
+        assertEquals(service.remain(-1), 1001); //очень странно, что вообще можно купить на -1рубль :)
+    }
+}
